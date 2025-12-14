fa33a11417ef99773da397a4c64c259ccf815dc4
yuuki1912 <yuuki1912@icloud.com>
2025-12-15 02:33:16 +0900

    frontendにredux,redux-saga,material-ui追加、ymlファイルにnginx追加

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,5 +2,5 @@
 if [ -d "./app" ]; then
   cd app && yarn start
 else
-  yarn create react-app app && cd app && yarn add react-router-dom && yarn start
+  yarn create react-app app && cd app && yarn add react-router-dom && yarn add redux && yarn add redux-saga && yarn add @mui/material @emotion/react @emotion/styled && yarn start
 fi
